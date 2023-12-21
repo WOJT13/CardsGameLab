ab4ad8a13f97d69176f3666039e65c57cedb9bc8
WOJT13 <wojtekpalka123@gmail.com>
2023-12-21 23:37:46 +0100

    New camera movement

--- CardsGame/Assets/Scripts/Game/CameraController.cs
+++ CardsGame/Assets/Scripts/Game/CameraController.cs
@@ -6,7 +6,7 @@ namespace Game
     /// Controls the camera's movement and rotation based on player input.
     /// </summary>
     public class CameraController : MonoBehaviour
-    {
+    {/*
         /// <summary>
         /// The speed at which the camera moves.
         /// </summary>
@@ -37,6 +37,45 @@ namespace Game
             if (Input.GetKey(KeyCode.E))
             {
                 transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+            }
+        }*/
+
+        private bool isDragging = false;
+        private Vector3 dragStartPosition;
+
+        // Ograniczenia kamery
+        public float minX = 0f;
+        public float maxX = 120f;
+        public float minY = 15f;
+        public float maxY = 40f;
+
+        void Update()
+        {
+            HandleMouseInput();
+        }
+
+        void HandleMouseInput()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                dragStartPosition = Input.mousePosition;
+            }
+            else if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+            }
+
+            if (isDragging)
+            {
+                Vector3 dragDelta = Input.mousePosition - dragStartPosition;
+                dragStartPosition = Input.mousePosition;
+
+                // Dostosuj przesunięcie kamery na podstawie przesunięcia myszy
+                float newX = Mathf.Clamp(Camera.main.transform.position.x - dragDelta.x * 0.1f, minX, maxX);
+                float newY = Mathf.Clamp(Camera.main.transform.position.y - dragDelta.y * 0.1f, minY, maxY);
+
+                Camera.main.transform.position = new Vector3(newX, newY, Camera.main.transform.position.z);
             }
         }
     }
